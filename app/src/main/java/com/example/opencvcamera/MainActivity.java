7f2bd4affe68bdb2774912f494480a946c2cf964
hp <codeankit05@gmail.com>
2021-04-02 00:43:03 +0530

    Capturing and saving images of detected faces

--- app/src/main/java/com/example/opencvcamera/MainActivity.java
+++ app/src/main/java/com/example/opencvcamera/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.opencvcamera;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -32,26 +42,40 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-    private static String TAG="MainActivity";
-     CascadeClassifier cascadeClassifier;
-     int width,height;
-    int absoluteFaceSize;
+    private static String TAG = "MainActivity";
+    CascadeClassifier cascadeClassifier;
+    public static final int detector = 0, detector1 = 1;
+    private int detect = detector;
+    int width, height;
+    int absoluteFaceSize = 0;
+    float relativeFaceSize = 0.2f;
     Button btn;
-    int a,i;
-
+    int a, i;
+    private cameraView cameraView;
+    int count = 0;
+    public static final int j = 0, k = 1;
+    public static final int train = 0, idle = 2;
+    private int state = idle;
+    int max = 10;
+    String path="";
     //Instance of java Camera View
     JavaCameraView javaCameraView;
-    Mat mRgba,m1,m2,imgGrey,imgCnny;
-    BaseLoaderCallback mLoaderCallBack=new BaseLoaderCallback(this) {
+    Mat mRgba, m1, m2, imgGrey, imgCnny;
+    private ImageView imageView;
+    Bitmap bitmap;
+    Handler handler;
+    ToggleButton toggleButton;
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            switch (status){
-                case BaseLoaderCallback.SUCCESS:{
-                    initializeOpenCVDependencies();break;
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    initializeOpenCVDependencies();
+                    break;
                     //javaCameraView.enableView();break;
                 }
 
-                default:{
+                default: {
                     super.onManagerConnected(status);
                     break;
 
@@ -81,17 +105,21 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
             // Load the cascade classifier
             cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            if (cascadeClassifier.empty()) {
+                cascadeClassifier = null;
+            } else
+                cascadeDir.delete();
         } catch (Exception e) {
             Log.e("OpenCVActivity", "Error loading cascade", e);
         }
 
 
         // And we are ready to go
-        javaCameraView.enableView();
+        cameraView.enableView();
     }
 
 
-    static{
+    static {
 
     }
 
@@ -101,14 +129,47 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
         //Binding Java Camera view in layout
-        javaCameraView=(JavaCameraView)findViewById(R.id.cam);
-        btn=(Button)findViewById(R.id.button);
-        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        cameraView = (cameraView) findViewById(R.id.cameraView);
+        btn = (Button) findViewById(R.id.button);
+        //javaCameraView.setVisibility(SurfaceView.VISIBLE);
         //setting a callback function
-        javaCameraView.setCvCameraViewListener(this);
+        cameraView.setCvCameraViewListener(this);
+        imageView = findViewById(R.id.image);
+        toggleButton = (ToggleButton) findViewById(R.id.toggle);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                captureOnClick();
+            }
+        });
+        path=getFilesDir()+"facedetect";
+        handler = new Handler() {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if (msg.obj == "IMG") {
+                    Canvas canvas = new Canvas();
+                    canvas.setBitmap(bitmap);
+                    imageView.setImageBitmap(bitmap);
+                    if (count > max - 1) {
+                        toggleButton.setChecked(false);
+                        captureOnClick();
+                    }
+                }
+            }
+        };
+        boolean success=(new File(path)).mkdirs();
+    }
 
-
-
+    void captureOnClick(){
+        if(toggleButton.isChecked())
+            state=train;
+        else
+        {
+            Toast.makeText(this,"captured",Toast.LENGTH_SHORT);
+            count=0;
+            state=idle;
+            imageView.setImageResource(R.drawable.ic_launcher_background);
+        }
 
     }
 
@@ -147,10 +208,10 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public void onCameraViewStarted(int width, int height) {
         mRgba=new Mat(height,width, CvType.CV_8UC4);
-        m1=new Mat(height,width, CvType.CV_8UC4);
-        m2=new Mat(height,width, CvType.CV_8UC4);
+      //  m1=new Mat(height,width, CvType.CV_8UC4);
+       // m2=new Mat(height,width, CvType.CV_8UC4);
         imgGrey=new Mat(height,width, CvType.CV_8UC1);
-        absoluteFaceSize=(int)(height*0.2);
+        //absoluteFaceSize=(int)(height*0.2);
 
 
 
@@ -159,8 +220,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public void onCameraViewStopped() {
         mRgba.release();
-        m1.release();
-        m2.release();
+//        m1.release();
+//        m2.release();
         imgGrey.release();
 
     }
@@ -169,29 +230,64 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRgba=inputFrame.rgba();
+        imgGrey=inputFrame.gray();
        //rotate the frame to 90
-       Core.transpose(mRgba,m1);
-   Imgproc.resize(m1,m2,m2.size(),0,0,0);
-        Core.flip(m2,mRgba,1);
-        Imgproc.cvtColor(mRgba,imgGrey,Imgproc.COLOR_RGB2GRAY);
+       //Core.transpose(mRgba,m1);
+        Mat mrgbat =mRgba.t();
+
+        Core.flip(mrgbat,mrgbat,1);
+        Imgproc.resize(mrgbat,mrgbat,mRgba.size());
+        //Imgproc.cvtColor(mRgba,imgGrey,Imgproc.COLOR_RGB2GRAY);
 
         // Use the classifier to detect faces
 
-        MatOfRect faces = new MatOfRect();
-        if (cascadeClassifier != null) {
-            cascadeClassifier.detectMultiScale(imgGrey, faces, 1.1, 2, 2,
-                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+        if(absoluteFaceSize==0){
+            int height=imgGrey.rows();
+            if(Math.round(height*relativeFaceSize)>0){
+                absoluteFaceSize=Math.round(height*relativeFaceSize);
+            }
         }
+
+        MatOfRect faces = new MatOfRect();
+        if(detect==detector) {
+
+            if (cascadeClassifier != null) {
+                cascadeClassifier.detectMultiScale(imgGrey, faces, 1.1, 2, 2,
+                        new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+                MediaStore.Images.Media.insertImage(getContentResolver(),bitmap,"facedetect","");
+            }
+        }
+        else if(detect==detector1){
+
+        }
+        else{
+
+        }
+
 
         // If there are any faces found, draw a rectangle around it
         Rect[] facesArray = faces.toArray();
+        if((facesArray.length==1)&&(state==train)&&(count<max)){
+            Mat mat;
+            Rect rect=facesArray[0];
+            mat=mRgba.submat(rect);
+            bitmap=Bitmap.createBitmap(mat.width(),mat.height(),Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(mat,bitmap);
+            Message message=new Message();
+            String text="IMG";
+            message.obj=text;
+            handler.sendMessage(message);
+            if(count<max){
+                count=count+1;
+            }
+        }
         for ( i = 0; i <facesArray.length; i++)
-            Core.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
+            Core.rectangle(mrgbat, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
         a=i;
 
 
 
-        return mRgba;
+        return mrgbat;
 
 
     }
